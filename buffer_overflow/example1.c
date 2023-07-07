98a7bc0eda6f376dae5f6d763f56672532de5c21
Arash Fotouhi <arash9k@gmail.com>
2023-07-07 05:27:08 -0700

    Good state for the sake of example.

--- buffer_overflow/example1.c
+++ buffer_overflow/example1.c
@@ -12,18 +12,22 @@ struct chamber {
 int main(void) {
 	struct chamber chamber1;
 	strcpy(chamber1.name, "alpha");
-	chamber1.pressure = 0xAB;
+	chamber1.pressure = 0xBB;
 	printmem(&chamber1);
+	printf("Chamber pressure: %X\n", chamber1.pressure);
 	printf("Enter new name for chamber:\n");
 	scanf("%s",chamber1.name);
 	printmem(&chamber1);
+	printf("Chamber pressure: %X\n", chamber1.pressure);
 	return 0;
 }
 
 void printmem(void* address) {
-	const unsigned int WINDOW = 30;
+	const unsigned int WINDOW = 20;
 	unsigned char* p = (unsigned char *)(address);
-	for (p -= WINDOW; p < (unsigned char *)(address + WINDOW); ++p) {
+	printf("--------------------------------\n");
+	for (p; p < (unsigned char *)(address + WINDOW); ++p) {
 		printf("%p: %02X (%c)\n", p, (unsigned int)*p, *p);
 	}
+	printf("--------------------------------\n");
 }
