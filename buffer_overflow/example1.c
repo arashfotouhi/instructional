abd6719b900455947a72e362f09b8653f8537a70
Arash Fotouhi <arash9k@gmail.com>
2023-07-06 18:19:58 -0700

    Add print mem.

--- buffer_overflow/example1.c
+++ buffer_overflow/example1.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printmem(void* start_address);
+
 int main(void) {
-	printf("Hi!\n");
+	char pass[10] = "password";
+	char name[16] = "noname";
+	printmem(name);
+	printf("Hi, What is your name?\n");
+	scanf("%s",name);
+	printf("Hi %s\n", name);
+	printf("Password is: %s\n", pass);
+	printmem(name);
 	return 0;
+}
+
+void printmem(void* address) {
+	const unsigned int WINDOW = 20;
+	char* p = (char *)(address);
+	for (p -= WINDOW; p < (char *)(address + WINDOW); ++p) {
+		printf("0x%08X: %c\n", p, *p);
+	}
 }
