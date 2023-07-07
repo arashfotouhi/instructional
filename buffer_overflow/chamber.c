c5739cc905e3870b2877cd9335a6714e80f31053
Arash Fotouhi <arash9k@gmail.com>
2023-07-07 06:10:26 -0700

    Add safety check functions.

--- buffer_overflow/chamber.c
+++ buffer_overflow/chamber.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printmem(void* start_address);
-
 struct chamber {
 	char name[15];
 	int pressure;
 };
+
+void printmem(void* start_address);
+int is_chamber_safe(const struct chamber *);
+void check_chamber(const struct chamber *);
 
 int main(void) {
 	struct chamber chamber1;
@@ -15,10 +17,12 @@ int main(void) {
 	chamber1.pressure = 0xBB;
 	printmem(&chamber1);
 	printf("Chamber pressure: %X\n", chamber1.pressure);
+	check_chamber(&chamber1);
 	printf("Enter new name for chamber:\n");
 	scanf("%s",chamber1.name);
 	printmem(&chamber1);
 	printf("Chamber pressure: %X\n", chamber1.pressure);
+	check_chamber(&chamber1);
 	return 0;
 }
 
@@ -30,4 +34,23 @@ void printmem(void* address) {
 		printf("%p: %02X (%c)\n", p, (unsigned int)*p, *p);
 	}
 	printf("--------------------------------\n");
+}
+
+int is_chamber_safe(const struct chamber *ch) {
+	const int SAFE_LEVEL = 0x44;
+	if (ch->pressure < SAFE_LEVEL) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void check_chamber(const struct chamber *ch) {
+	if (is_chamber_safe(ch)) {
+		printf("Chamber is safe.\n");
+	}
+	else {
+		printf("CHAMBER PRESSURE TOO HIGH!!!!\n");
+	}
 }
